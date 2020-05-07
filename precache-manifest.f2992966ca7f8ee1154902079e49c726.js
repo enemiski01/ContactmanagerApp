dd14d42db1d791c28a2447936712ea96f4e22d01
Your NameEnemiski01 <adamaenema@gmail.com>
2020-05-07 02:29:41 -0700

    Updates

---
+++ precache-manifest.f2992966ca7f8ee1154902079e49c726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675e3d0eb06247d4a1bf00f5cc7aee94",
+    "url": "/ContactmanagerApp/index.html"
+  },
+  {
+    "revision": "995f791756aa69753274",
+    "url": "/ContactmanagerApp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "995f791756aa69753274",
+    "url": "/ContactmanagerApp/static/js/2.e9598a36.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ContactmanagerApp/static/js/2.e9598a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09856d8e2eb24c6e9e0c",
+    "url": "/ContactmanagerApp/static/js/main.f5b177a8.chunk.js"
+  },
+  {
+    "revision": "3a3bb221121f2a4bb0e9",
+    "url": "/ContactmanagerApp/static/js/runtime-main.4963ea8d.js"
+  }
+]);
